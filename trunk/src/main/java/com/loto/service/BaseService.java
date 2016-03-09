39a3ffe51c9a84b47231f7788e12bf8c09c32533
qmaolong <qmaolong@3982b50f-f787-4250-b0bf-74fa38a12756>
2016-03-09 09:46:05 +0000

    调通共用底层代码

--- trunk/src/main/java/com/loto/service/BaseService.java
+++ trunk/src/main/java/com/loto/service/BaseService.java
@@ -7,40 +7,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.github.pagehelper.PageInfo;
 import com.loto.dao.BaseDao;
 
-public class BaseService<D extends Serializable, M> {
-	private BaseDao<D, M> dao;
+public abstract class BaseService<D extends Serializable, M> {
 	protected D entity;
 	protected M example;
 	
 	public D findById(Long id){
-		return dao.selectByPrimaryKey(id);
+		return (D) getBaseDao().selectByPrimaryKey(id);
 	}
 	
 	public PageInfo<D> findByExample(M exam, Integer pageNum, Integer pageSize){
-		List<D> list = dao.selectByExample(exam);
+		List<D> list = getBaseDao().selectByExample(exam);
 		PageInfo<D> pageInfo = new PageInfo<D>(list);
 		
 		return pageInfo;
 	}
 	
 	public int save(D record){
-		return dao.insert(record);
+		return getBaseDao().insert(record);
 	}
 	
 	public int update(D record) {
-		return dao.updateByPrimaryKey(record);
+		return getBaseDao().updateByPrimaryKey(record);
 	}
 	
 	public int deleteByExample(M exam){
-		return dao.deleteByExample(exam);
+		return getBaseDao().deleteByExample(exam);
 	}
 	
 	public int deleteById(Long id) {
-		return dao.deleteByPrimaryKey(id);
+		return getBaseDao().deleteByPrimaryKey(id);
 	}
 	
-	public void setBaseDao(BaseDao<D, M> baseDao) {
-		this.dao = baseDao;
-	}
+	protected abstract BaseDao<D, M> getBaseDao();
 
 }
