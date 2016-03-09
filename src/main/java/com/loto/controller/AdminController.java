5b7866f15d00ec9409b2446429569fcaf35f5053
qmaolong <qmaolong@3982b50f-f787-4250-b0bf-74fa38a12756>
2016-03-09 09:46:05 +0000

    调通共用底层代码

--- src/main/java/com/loto/controller/AdminController.java
+++ src/main/java/com/loto/controller/AdminController.java
@@ -1,14 +1,26 @@
 package com.loto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.loto.model.Admin;
 import com.loto.model.AdminExample;
+import com.loto.service.AdminService;
 
 @Controller
 @RequestMapping("admin")
-public class AdminController extends BaseController<Admin, AdminExample>{
+public class AdminController {
+	@Autowired
+	private AdminService adminService;
+	
+	@RequestMapping("list")
+	public String list(String queryString, Integer pageNum, Integer pageSize){
+		AdminExample exam = new AdminExample();
+		adminService.findByExample(exam, pageNum, pageSize);
+		
+		return "";
+	}
 	
 
 }
--- src/main/java/com/loto/service/AdminService.java
+++ src/main/java/com/loto/service/AdminService.java
@@ -1,14 +1,22 @@
 package com.loto.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.loto.dao.AdminMapper;
+import com.loto.dao.BaseDao;
 import com.loto.model.Admin;
 import com.loto.model.AdminExample;
 
 @Component
 @Transactional
 public class AdminService extends BaseService<Admin, AdminExample>{
+	@Autowired
+	private AdminMapper adminDao;
+	
+	protected BaseDao<Admin, AdminExample> getBaseDao(){
+		return adminDao;
+	}
 	
 }
